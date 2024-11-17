bbcdb9bd3508f572935721ca45d19ea2231869cc
mintuky1508 <mintuky1508@gmail.com>
2024-11-18 00:59:01 +0530

    Delete modules directory

--- modules/ec2/main.tf
+++
@@ -1,9 +0,0 @@
-# resource "aws_instance" "ec2" {
-#   ami           = var.ami_id
-#   instance_type = var.instance_type
-#   key_name      = var.key_name
-
-#   tags = {
-#     Name = var.instance_name
-#   }
-# }
--- modules/s3/main.tf
+++
@@ -1,4 +0,0 @@
-# resource "aws_s3_bucket" "bucketttt" {
-#   bucket        = var.buckett_name
-  
-# }
